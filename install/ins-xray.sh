4ce38191a66395a1a9748806fdd1f5f430b79f8a
freetunnel <153220342+freetunnel@users.noreply.github.com>
2024-12-10 13:53:56 +0700

    Update ins-xray.sh

--- install/ins-xray.sh
+++ install/ins-xray.sh
@@ -430,15 +430,10 @@ EOF
 cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
              listen 80;
-             listen [::]:80;
              listen 7788;
-             listen [::]:7788;
              listen 8181;
-             listen [::]:8181;
              listen 8282;
-             listen [::]:8282;
              listen 443 ssl http2 reuseport;
-             listen [::]:443 http2 reuseport;
              server_name *.$domain;
              ssl_certificate /etc/xray/xray.crt;
              ssl_certificate_key /etc/xray/xray.key;
